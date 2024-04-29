1ed5e7363e9ff086543b911c8b13119c397e2ba2
thales-b <thalesbouraoui@gmail.com>
2024-04-30 00:52:42 +0200

    init

---
+++ calculator/calculator.go
@@ -0,0 +1,14 @@
+// Package calculator does simple calculations.
+package calculator
+
+// Add takes two numbers and returns the result of adding
+// them together.
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+// Subtract takes two numbers a and b, and
+// returns the result of subtracting b from a.
+func Subtract(b, a float64) float64 {
+	return b - a
+}
