46c191abddfdce93901b71ec1df88f2439d33b1d
thales-b <thalesbouraoui@gmail.com>
2024-05-01 01:06:31 +0200

    test coverage

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,8 +1,18 @@
 package bookstore
 
+import "errors"
+
 // Book represents information about a book.
 type Book struct {
 	Title  string
 	Author string
 	Copies int
+}
+
+func Buy(b Book) (Book, error) {
+	if b.Copies == 0 {
+		return Book{}, errors.New("no copies left")
+	}
+	b.Copies--
+	return b, nil
 }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -1,6 +1,7 @@
 package bookstore_test
 
 import (
+	"bookstore"
 	"testing"
 )
 
@@ -10,5 +11,36 @@ func TestBook(t *testing.T) {
 		Title:  "Spark Joy",
 		Author: "Marie Kondo",
 		Copies: 2,
+	}
+}
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+	want := 1
+	result, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+	if want != got {
+		t.Errorf("had %d copies, want %d left, got %d left", b.Copies, want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 0,
+	}
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Error("want error buying from zero copies, got nil")
 	}
 }
